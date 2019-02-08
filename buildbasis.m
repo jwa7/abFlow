bd27465aad238304ddfb42e219eb630727b00fba
Joe Abbott <jwa7@uw.edu>
2019-02-07 23:44:48 -0800

    "Halfway complete build basis"

--- buildbasis.m
+++ buildbasis.m
@@ -18,10 +18,10 @@
 %     basis(p).N      array of normalization constants
 
 function buildbasis(atoms,xyz_a0,basissetdef)
-    s =    [0 0 0];
-    p_x =  [1 0 0];
-    p_y =  [0 1 0];
-    p_z =  [0 0 1];
+    s    = [0 0 0];
+    p_x  = [1 0 0];
+    p_y  = [0 1 0];
+    p_z  = [0 0 1];
     d_x2 = [2 0 0];
     d_xy = [1 1 0];
     d_xz = [1 0 1];
@@ -31,39 +31,78 @@ function buildbasis(atoms,xyz_a0,basissetdef)
     
     basis = struct;
     
-    for p=1:numel(atoms) % iterating over each atom in the atom list of the molecule
-       
-        basis(p).atom = atoms(p);
-        basis(p).A = xyz_a0(p,:);
+    for K=1:numel(atoms) % iterating over each atom in the atom list
         
-        basis(p).a = [];
+        cart_exp = [];
         
-        for i=1:numel(basissetdef{p}(i).shelltype) % iterating over 
+        for i=1:numel(basissetdef{K}.shelltype) % iterating over shell types 
+                                                % (i.e. 'S' then 'SP'..) 
             
-            type = basissetdef{p}(i).shelltype;
+            isSP = false;
+            type = basissetdef{K}(i).shelltype;
+            rad_exp = {};
             
             if type == 'S'
-                cart_exp = [s];
+                subshells = [s];
+                rad_exp{i} = basissetdef{K}(i).exponents;
             elseif type == 'SP'
-                cart_exp = [s; p_x; p_y; p_z];
+                subshells = [s; p_x; p_y; p_z];
+                isSP = true;
             elseif type == 'P'
-                cart_exp = [p_x; p_y; p_z];
+                subshells = [p_x; p_y; p_z];
             elseif type == 'D'
-                cart_exp = [d_x2; d_xy; d_xz; d_y2; d_yz; d_z2];
+                subshells = [d_x2; d_xy; d_xz; d_y2; d_yz; d_z2];
             end     
             
-            basis(p).a = [basis(p).a; cart_exp];
+            cart_exp = [cart_exp; subshells]; % px3 array of the cartesian exponenets
             
         end
+        
+        [m,n] = size(cart_exp); % number of basis functions given by m.
+                                % (number of rows in cartesian
+                                % exponent array)
+        
+        
+                                
+                                
+        for p=1:m
+            basis(p).atom = atoms(K);   % all m basis functions for atom K must be atom K
+            basis(p).A = xyz_a0(K,:);   % same principle, but for nuclear coordinates
+            basis(p).a = cart_exp(p,:); % each row in cart_exp corresponds to cartesian 
+                                        % coordinate of the mth basis function
             
-        
-        shelltype = basissetdef{p}.shelltype;
-        exponents = basissetdef{p}.exponents;
-        coeffs = basissetdef{p}.coeffs;
-        
+            for i=1:numel(basissetdef{K}.shelltype)
+                q=1;
+                for j=1:m
+                    
+                    if type == 'S'
+                        rad_exp{q} = basissetdef{K}(i).exponents;
+                        q=q+1;
+                    elseif type == 'SP'
+                        for t=1:4
+                            rad_exp{q} = bas
+                        end
+                    elseif type == 'P'
+                    elseif type == 'D'
+                    end 
+                end
+            end
+        end
+            
+            if isSP == false
+                basis(p).alpha = basissetdef{K}(i).exponents; 
+            end
+        end
+            
+            
+            basis(K).a = [basis(K).a; subshells];
+            basis(K).alpha = rad_exp;
+            basis(K).d = [basis(K).d; coeffs];
+            
+        end      
+          
     end
-
-
-
+    
     return basis;
+    
 end
