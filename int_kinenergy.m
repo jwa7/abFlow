14768113dfe0ccd30ddc655a269defac24aa8bac
Joe Abbott <jwa7@uw.edu>
2019-02-12 15:18:52 -0800

    "Uploading finished overlap integral and partially finishd kinetic energy integral."

--- int_kinenergy.m
+++ int_kinenergy.m
@@ -9,7 +9,6 @@ function T = int_kinenergy(basis)
 
 M = numel(basis);           % total number of basis funcs in basis
 T = zeros(M);               % initializing T as all-zero MxM matrix
-fac2 = @(n) prod(n:-2:1);   % evaluates the double factorial of n
 
 for mu=1:M     % iterates over all basis functions in basis
     
@@ -25,17 +24,16 @@ for mu=1:M     % iterates over all basis functions in basis
         b = basis(nu).a;
         numPrimB = numel(basis(nu).alpha);  % number of primitives in
                                             % nu-th basis function.
-        T_mu_nu = 0;
         
         for k=1:numPrimA        % iterating over mu's primitives
             
-            alpha_k = basis(mu).alpha(k);   % mu's k-th radial exponent
+            alpha = basis(mu).alpha(k);   % mu's k-th radial exponent
             d_k = basis(mu).d(k);   % mu's k-th contraction coefficient
             N_k = basis(mu).N(k);  % mu's k-th normalization constant
             
             for l=1:numPrimB        % iterating over nu's primitives
                 
-                beta_l = basis(nu).alpha(l);    % nu's l-th radial exponent
+                beta = basis(nu).alpha(l);    % nu's l-th radial exponent
                 d_l = basis(nu).d(l);   % nu's l-th contraction coefficient
                 N_l = basis(nu).N(l);  % nu's l-th normalization constant
                 
@@ -43,21 +41,22 @@ for mu=1:M     % iterates over all basis functions in basis
                 
                 for omega=1:3   % iterating over components x, y, z
                     I_omega = 0;
-                    
-                    if b >= 2
-                        I_omega = beta_l*(2*b(omega)+1)*integral_3D(alpha_k,beta_l,A,B,a,b) ...
-                                  -2*beta_l^2*integral_3D(alpha_k,beta_l,A,B,a,b+2);
-                                  -0.5*b(omega)*(b(omega)-1)*integral_3D(alpha_k,beta_l,A,B,a,b-2);
+                    sub_term = 0;
+                    if b(omega) >= 2
+                        b(omega) = b(omega)-2;
+                        sub_term = -0.5*b(omega)*(b(omega)-1)*overlap_primitive(a,b,alpha,beta,A,B);
                     end
+                    I_omega = beta*(2*b(omega)+1)*overlap_primitive(a,b,alpha,beta,A,B) ...
+                        -2*beta^2*overlap_primitive(a,b+2,alpha,beta,A,B) + sub_term;
+                    
+                    
                     
                     I3D_kinetic = I3D_kinetic + I_omega;
                     
                 end
-                T_mu_nu = T_mu_nu + d_k*d_l * N_k*N_l * I3D_kinetic;
+                T(mu,nu) = T(mu,nu) + d_k*d_l * N_k*N_l * I3D_kinetic;
             end
         end
-        T(mu, nu) = T_mu_nu;
     end
-    
 end
 end
--- int_overlap.m
+++ int_overlap.m
@@ -9,7 +9,6 @@ function S = int_overlap(basis)
 
 M = numel(basis);           % total number of basis funcs in basis
 S = zeros(M);               % initializing S as all-zero MxM matrix
-fac2 = @(n) prod(n:-2:1);   % evaluates the double factorial of n
 
 for mu=1:M     % iterates over all basis functions in basis
     
@@ -25,28 +24,25 @@ for mu=1:M     % iterates over all basis functions in basis
         b = basis(nu).a;
         numPrimB = numel(basis(nu).alpha);  % number of primitives in
                                             % nu-th basis function.
-        S_mu_nu = 0;
         
         for k=1:numPrimA        % iterating over mu's primitives
             
-            alpha_k = basis(mu).alpha(k);   % mu's k-th radial exponent
+            alpha = basis(mu).alpha(k);   % mu's k-th radial exponent
             d_k = basis(mu).d(k);   % mu's k-th contraction coefficient
             N_k = basis(mu).N(k);  % mu's k-th normalization constant
             
             for l=1:numPrimB        % iterating over nu's primitives
                 
-                beta_l = basis(nu).alpha(l);    % nu's l-th radial exponent
+                beta = basis(nu).alpha(l);    % nu's l-th radial exponent
                 d_l = basis(nu).d(l);   % nu's l-th contraction coefficient
                 N_l = basis(nu).N(l);  % nu's l-th normalization constant
-                                                    
-                I3D = integral_3D(alpha_k,beta_l,A,B,a,b);
                 
-                S_mu_nu = S_mu_nu + d_k*d_l * N_k*N_l * I3D;
+                Sp = overlap_primitive(a,b,alpha,beta,A,B); 
+                
+                S(mu,nu) = S(mu,nu) + d_k*d_l * N_k*N_l * Sp; % matrix element 
+                                                              % is a cumulative sum
             end
         end
-        S(mu, nu) = S_mu_nu;
     end
-    
 end
-
 end
