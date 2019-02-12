4ac9a01b71074e42f9f8c8f30096239788af8d4a
Joe Abbott <jwa7@uw.edu>
2019-02-11 22:41:06 -0800

    "Final changes made overlap integral; modification of style in primitive overlap code."

--- int_overlap.m
+++ int_overlap.m
@@ -6,65 +6,67 @@
 %       S           M×M matrix of overlap integrals
 
 function S = int_overlap(basis)
+
+M = numel(basis);           % total number of basis funcs in basis
+S = zeros(M);               % initializing S as all-zero MxM matrix
+fac2 = @(n) prod(n:-2:1);   % evaluates the double factorial of n
+
+for mu=1:M     % iterates over all basis functions in basis
     
-    M = numel(basis);           % total number of basis funcs in basis
-    S = zeros(M);               % initializing S as all-zero MxM matrix
-    fac2 = @(n) prod(n:-2:1);   % evaluates the double factorial of n
-
-    for mu=1:M     % iterates over basis functions in basis
+    A = basis(mu).A;
+    a = basis(mu).a;
+    numPrimA = numel(basis(mu).alpha);  % number of primitives in
+                                        % the mu-th basis function.
+    
+    for nu=1:M      % iterates over basis funcs in basis again to
+                    % compare each func with every other one
         
-        A = basis(mu).A;
-        a = basis(mu).a;
-        numPrimA = numel(basis(mu).alpha);  % number of primitives in
-                                            % the mu-th basis function. 
-                                           
-        for nu=1:M      % iterates over basis funcs in basis again to
-                        % compare each func with every other one
-
-            B = basis(nu).A;
-            b = basis(nu).a;
-            numPrimB = numel(basis(nu).alpha);  % number of primitives in
-                                                % nu-th basis function. 
-            S_mu_nu = 0;
-                                        
-            for k=1:numPrimA        % iterating over mu's primitives
+        B = basis(nu).A;
+        b = basis(nu).a;
+        numPrimB = numel(basis(nu).alpha);  % number of primitives in
+                                            % nu-th basis function.
+        S_mu_nu = 0;
+        
+        for k=1:numPrimA        % iterating over mu's primitives
             
             alpha_k = basis(mu).alpha(k);   % mu's k-th radial exponent
             d_k = basis(mu).d(k);   % mu's k-th contraction coefficient
             N_k = basis(mu).N(k);  % mu's k-th normalization constant
-                                                
-                for l=1:numPrimB        % iterating over nu's primitives
-                    
-                    beta_l = basis(nu).alpha(l);    % nu's l-th radial exponent
-                    d_l = basis(nu).d(l);   % nu's l-th contraction coefficient
-                    N_l = basis(nu).N(l);  % nu's l-th normalization constant
-
-                    p_kl = alpha_k + beta_l;
-                    K_AB = exp(-alpha_k*beta_l*norm(A-B)^2/(alpha_k+beta_l));
-                    Integral_3D = K_AB*(pi/p_kl)^(3/2); % initializing the 3D integral 
-                                                        % with the prefactors
-                    
-                    for omega=1:3   % iterating over components x, y, z
-                        P_omega = (alpha_k.*A(omega) + beta_l.*B(omega))./(alpha_k + beta_l);
-                        I_omega = 0;    % initializing the 1D integral 
-                        for i=0:(a(omega)+b(omega))/2
-                            q = 2*i;
-                            f_q = 0;
-                            for j=max(0,q-a(omega)):min(q,b(omega))
-                                f_q = f_q + nchoosek(a(omega),q-j)*nchoosek(b(omega),j)*(P_omega-A(omega))^(a(omega)-q+j)*(P_omega-B(omega))^(b(omega)-j);
-                            end
-                            I_omega = I_omega + f_q*fac2(q-1)/(2*p_kl)^i;
-                        end 
-                        Integral_3D = Integral_3D * I_omega;    % the 3D intergal is a cumulative 
-                                                                % product of the three 1D components
-                    end
-                    S_mu_nu = S_mu_nu + d_k*d_l * N_k*N_l * Integral_3D;
-                end
+            
+            for l=1:numPrimB        % iterating over nu's primitives
                 
+                beta_l = basis(nu).alpha(l);    % nu's l-th radial exponent
+                d_l = basis(nu).d(l);   % nu's l-th contraction coefficient
+                N_l = basis(nu).N(l);  % nu's l-th normalization constant
+                
+                p_kl = alpha_k + beta_l;
+                K_AB = exp(-alpha_k*beta_l*norm(A-B)^2/(alpha_k+beta_l));
+                Integral_3D = K_AB*(pi/p_kl)^(3/2); % initializing the 3D integral
+                                                    % with the prefactors.
+                
+                for omega=1:3   % iterating over components x, y, z
+                    
+                    P_omega = (alpha_k.*A(omega) + beta_l.*B(omega))./(alpha_k + beta_l);
+                    I_omega = 0;    % initializing the 1D integral
+                    
+                    for i=0:(a(omega)+b(omega))/2
+                        
+                        q = 2*i;
+                        f_q = 0;
+                        for j=max(0,q-a(omega)):min(q,b(omega))
+                            f_q = f_q + nchoosek(a(omega),q-j)*nchoosek(b(omega),j)*(P_omega-A(omega))^(a(omega)-q+j)*(P_omega-B(omega))^(b(omega)-j);
+                        end
+                        I_omega = I_omega + f_q*fac2(q-1)/(2*p_kl)^i;
+                    end
+                    Integral_3D = Integral_3D * I_omega;    % the 3D intergal is a cumulative
+                                                            % product of the three 1D components
+                end
+                S_mu_nu = S_mu_nu + d_k*d_l * N_k*N_l * Integral_3D;
             end
-            S(mu, nu) = S_mu_nu;
         end
-        
+        S(mu, nu) = S_mu_nu;
     end
     
+end
+
 end
--- overlap_primitive.m
+++ overlap_primitive.m
@@ -12,8 +12,8 @@ function Sp = overlap_primitive(a,b,alpha,beta,A,B)
 
 df = @(n) prod(n:-2:1); %calculates double factorial of n
 
-%Terms for calculating 3D overlap integral that do not 
-p = alpha + beta; 
+%Terms for calculating 3D overlap integral that do not
+p = alpha + beta;
 P = (alpha.*A + beta.*B)/(p);
 K_AB = exp(((-alpha*beta)/(alpha+beta))*norm(A - B)^2);
 
@@ -23,21 +23,19 @@ for w = 1:3  %given a = [ax ay az], w loops through ax ay then az, and calculate
     for i=0:(a(w)+b(w)/2) %This loop sums over i=0 to (a(w) + b(w))/2 for calculating Iw, the 1D integral
         k = 2*i;
         fk = 0;
-        for j = max(0,k-a(w)) : min(k,b(w))    %This loop calculates fk_temp and sums them into fk, which is used to calculate the 1D integral Iw.    
-            binom_coeff1 = factorial(a(w)) / (factorial(k-j)*factorial(a(w) - (k-j))); %Calculates 1st binomial coefficient term
-            binom_coeff2 = factorial(b(w)) / (factorial(j)*factorial(b(w)-j)); %Calculates 2nd binomial coefficient term
-            term3 = (P(w) - A(w))^(a(w) - k + j); 
-            term4 = (P(w) - B(w))^(b(w) - j); 
+        for j = max(0,k-a(w)) : min(k,b(w))    %This loop calculates fk_temp and sums them into fk, which is used to calculate the 1D integral Iw.
+            binom_coeff1 = nchoosek(a(w),k-j);  % Calculates 1st binomial coefficient
+            binom_coeff2 = nchoosek(b(w),j);    % Calculates 2nd binomial coefficient
+            term3 = (P(w) - A(w))^(a(w) - k + j);
+            term4 = (P(w) - B(w))^(b(w) - j);
             fk_temp = binom_coeff1 * binom_coeff2 * term3 * term4; %a single fk term
-            fk = fk + fk_temp; %Sum of fk terms 
+            fk = fk + fk_temp; %Sum of fk terms
         end
         Iw_temp = fk*df(2*i-1)/((2*p).^i); %A single 1D integral
         Iw(w) = Iw(w) + Iw_temp; %Sum of the 1D integrals stored according to the appropriate coordinate (ax, ay, or az)
     end
 end
 Sp = (pi/p)^(3/2) * K_AB * Iw(1) * Iw(2) * Iw(3); %3D overlap integral.
-
-
 
 end
 
