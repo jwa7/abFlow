a32f64e49caa0a70e438071b569978f65950fd2e
Joe Abbott <jwa7@uw.edu>
2019-03-01 01:17:01 -0800

    making some changes to variable names

--- eval_bf.m
+++ eval_bf.m
@@ -1,3 +1,13 @@
+% Evaluates a basis function at a list of positions in space.
+% 
+% Input:
+%     basisfun    structure with basis function information, 
+%                 from list of basis functions.
+%     xyz_a0      mx3 list of Cartesian coordinates, one point per row, 
+%                 over which to evaluate the basis function.
+% Output:
+%     val         m-element array, with the values of the basis function.
+
 function val = eval_bf(basisfun,xyz_a0)
 a = basisfun.a;
 alpha = basisfun.alpha;
@@ -8,7 +18,7 @@ evalbf = zeros(size(xyz_a0,1),1);
 
 for i=1:size(xyz_a0,1)
    
-    for j=1:length(alpha);
+    for j=1:length(alpha)
         evalbf(i) = evalbf(i) + N(j)*coeffs(j).*exp(-alpha(j)*(sum((xyz_a0(i,:)-A).^2)));
     end
     evalbf(i) = evalbf(i)*((xyz_a0(i,1)-A(1)).^a(1) .* (xyz_a0(i,2)-A(2)).^a(2) .* (xyz_a0(i,3)-A(3)).^a(3));   
